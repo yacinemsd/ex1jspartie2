1b67816bfab4fc81ba26b11bf46a49c3022c63bd
yacinemsd <messaoudi.y76@gmail.com>
2018-10-10 16:58:31 +0200

    finish

--- ex3.js
+++
@@ -1,6 +0,0 @@
-var lastname = document.getElementById("lastname");
-
-lastname.addEventListener("keyup", function (event){
- var lastnamevalue = document.getElementById('lastname').value;
-            alert(lastnamevalue);
-        })
---
+++ exercice5/ex5.js
@@ -0,0 +1,54 @@
+document.getElementById('image1').addEventListener('mouseover', function (){
+
+document.getElementById('image1').src='images/image1_2.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+document.getElementById('image1').addEventListener('mouseout', function (){
+
+document.getElementById('image1').src='images/image1.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+
+document.getElementById('image2').addEventListener('mouseover', function (){
+
+document.getElementById('image2').src='images/image2_2.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+document.getElementById('image2').addEventListener('mouseout', function (){
+
+document.getElementById('image2').src='images/image2.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+
+document.getElementById('image3').addEventListener('mouseover', function (){
+
+document.getElementById('image3').src='images/image3_2.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+document.getElementById('image3').addEventListener('mouseout', function (){
+
+document.getElementById('image3').src='images/image3.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+
+document.getElementById('image4').addEventListener('mouseover', function (){
+
+document.getElementById('image4').src='images/image4_2.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+document.getElementById('image4').addEventListener('mouseout', function (){
+
+document.getElementById('image4').src='images/image4.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+
+document.getElementById('image5').addEventListener('mouseover', function (){
+
+document.getElementById('image5').src='images/image5_2.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
+document.getElementById('image5').addEventListener('mouseout', function (){
+
+document.getElementById('image5').src='images/image5.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
