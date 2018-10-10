d6928687ad7c508457db4746f98419ac2870df8c
yacinemsd <messaoudi.y76@gmail.com>
2018-10-10 15:59:15 +0200

    raz

--- exercice1/ex1.js
+++ exercice1/ex1.js
@@ -1,6 +1,10 @@
-function changeimg(img){
+document.getElementById('image1').addEventListener('mouseover', function (){
 
-document.getElementById('image1').src=img;
+document.getElementById('image1').src='images/image1_2.jpg'; //changement de la valeur src de l'éléement id='image1'
 
-}
- 
+})
+document.getElementById('image1').addEventListener('mouseout', function (){
+
+document.getElementById('image1').src='images/image1.jpg'; //changement de la valeur src de l'éléement id='image1'
+
+})
---
+++ exercice4/ex4.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName('button')[0].addEventListener('click', function() {
+  var lastname = document.getElementById('lastname');
+  var firstname = document.getElementById('firstname');
+  var city = document.getElementById('city');
+  lastname.value = '';
+  firstname.value = '';
+  city.value = '';
+}
+)
