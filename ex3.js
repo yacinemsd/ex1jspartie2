33ae8c9e9e5cc11df0f876ac2ef734fd9486e83d
yacinemsd <messaoudi.y76@gmail.com>
2018-10-10 12:08:06 +0200

    keyup

---
+++ ex3.js
@@ -0,0 +1,6 @@
+var lastname = document.getElementById("lastname");
+
+lastname.addEventListener("keyup", function (event){
+ var lastnamevalue = document.getElementById('lastname').value;
+            alert(lastnamevalue);
+        })
